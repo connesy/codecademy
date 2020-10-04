e2f9c0e555796a6a057059bd840ba9a308f471e9
Stefan Mejlgaard <connesy@gmail.com>
2020-10-04 02:52:07 +0200

    Fix liniting

--- JavaScript/src/rock-paper-scissors/app.js
+++ JavaScript/src/rock-paper-scissors/app.js
@@ -1,68 +1,68 @@
 #!/usr/bin/env node
 
 function UserException(message) {
-  this.message = message;
-  this.name = 'UserException';
+  this.message = message
+  this.name = 'UserException'
 }
 
 function isEntryPoint() {
-  return require.main === module;
+  return require.main === module
 }
 
-const prompt = require('prompt-sync')({sigint: true});
-const validChoices = ['rock', 'paper', 'scissors'];
+const prompt = require('prompt-sync')({ sigint: true })
+const validChoices = ['rock', 'paper', 'scissors']
 
 function getUserChoice() {
-  const userInput = prompt("Pick one of 'rock', 'paper' or 'scissors': ");
-  const userChoice = userInput.toLowerCase();
+  const userInput = prompt("Pick one of 'rock', 'paper' or 'scissors': ")
+  const userChoice = userInput.toLowerCase()
 
   if ([...validChoices, 'bomb'].includes(userChoice)) {
-    return userChoice;
+    return userChoice
   } else {
-    throw new UserException(`"${userInput}" is not a valid choice`);
-  };
-};
+    throw new UserException(`"${userInput}" is not a valid choice`)
+  }
+}
 
 function getRandomChoice() {
-  const randomIndex = Math.floor(Math.random() * validChoices.length);
-  const randomChoice = validChoices[randomIndex];
-  return randomChoice;
-};
+  const randomIndex = Math.floor(Math.random() * validChoices.length)
+  const randomChoice = validChoices[randomIndex]
+  return randomChoice
+}
 
 function determineWinner(computerChoice, userChoice) {
   switch (userChoice) {
     case 'bomb':
-      return 'The user won (by cheating)!';
+      return 'The user won (by cheating)!'
 
     case computerChoice:
-      return 'The game is a tie';
+      return 'The game is a tie'
 
     case 'rock':
-      return (computerChoice === 'paper') ? 'The computer won!' : 'The user won!';
+      return computerChoice === 'paper' ? 'The computer won!' : 'The user won!'
 
     case 'paper':
-      return (computerChoice === 'scissors') ? 'The computer won!' : 'The user won!';
+      return computerChoice === 'scissors' ? 'The computer won!' : 'The user won!'
 
     case 'scissors':
-      return (computerChoice === 'rock') ? 'The computer won!' : 'The user won!';
-  };
-};
+      return computerChoice === 'rock' ? 'The computer won!' : 'The user won!'
+  }
+}
 
 function playRound() {
-  const userChoice = getUserChoice();
-  const computerChoice = getRandomChoice();
-  const message = determineWinner(computerChoice, userChoice);
-  console.log(message);
-  return message;
+  const userChoice = getUserChoice()
+  const computerChoice = getRandomChoice()
+  const message = determineWinner(computerChoice, userChoice)
+  console.log(message)
+  return message
 }
 
 // Entrypoint
 if (isEntryPoint()) {
-  playRound();
+  playRound()
 }
 
 module.exports = {
   validChoices,
   getRandomChoice,
-  determineWinner
-};
+  determineWinner,
+}
--- JavaScript/src/rock-paper-scissors/app.test.js
+++ JavaScript/src/rock-paper-scissors/app.test.js
@@ -3,61 +3,61 @@
 const { validChoices, getRandomChoice, determineWinner } = require('./app')
 
 test('Get random choice returns valid choice', () => {
-  expect(validChoices).toContain(getRandomChoice());
-});
+  expect(validChoices).toContain(getRandomChoice())
+})
 
 describe('determineWinner', () => {
   describe('Computer plays rock', () => {
-    const computerRock = 'rock';
+    const computerRock = 'rock'
 
     test('User plays rock', () => {
-      expect(determineWinner(computerRock, 'rock')).toContain('tie');
-    });
+      expect(determineWinner(computerRock, 'rock')).toContain('tie')
+    })
 
     test('User plays paper', () => {
-      expect(determineWinner(computerRock, 'paper')).toContain('user');
-    });
+      expect(determineWinner(computerRock, 'paper')).toContain('user')
+    })
 
     test('User plays scissors', () => {
-      expect(determineWinner(computerRock, 'scissors')).toContain('computer');
-    });
-  });
+      expect(determineWinner(computerRock, 'scissors')).toContain('computer')
+    })
+  })
 
   describe('Computer plays paper', () => {
-    const computerPaper = 'paper';
+    const computerPaper = 'paper'
 
     test('User plays rock', () => {
-      expect(determineWinner(computerPaper, 'rock')).toContain('computer');
-    });
+      expect(determineWinner(computerPaper, 'rock')).toContain('computer')
+    })
 
     test('User plays paper', () => {
-      expect(determineWinner(computerPaper, 'paper')).toContain('tie');
-    });
+      expect(determineWinner(computerPaper, 'paper')).toContain('tie')
+    })
 
     test('User plays scissors', () => {
-      expect(determineWinner(computerPaper, 'scissors')).toContain('user');
-    });
-  });
+      expect(determineWinner(computerPaper, 'scissors')).toContain('user')
+    })
+  })
 
   describe('Computer plays scissors', () => {
-    const computerScissors = 'scissors';
+    const computerScissors = 'scissors'
 
     test('User plays rock', () => {
-      expect(determineWinner(computerScissors, 'rock')).toContain('user');
-    });
+      expect(determineWinner(computerScissors, 'rock')).toContain('user')
+    })
 
     test('User plays paper', () => {
-      expect(determineWinner(computerScissors, 'paper')).toContain('computer');
-    });
+      expect(determineWinner(computerScissors, 'paper')).toContain('computer')
+    })
 
     test('User plays scissors', () => {
-      expect(determineWinner(computerScissors, 'scissors')).toContain('tie');
-    });
-  });
+      expect(determineWinner(computerScissors, 'scissors')).toContain('tie')
+    })
+  })
 
   test('User wins with bomb', () => {
     validChoices.forEach((computerChoice) => {
-      expect(determineWinner(computerChoice, 'bomb')).toContain('cheating');
-    });
-  });
-});
+      expect(determineWinner(computerChoice, 'bomb')).toContain('cheating')
+    })
+  })
+})
